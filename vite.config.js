5d6c2244c43f648f152772e6d85d372408a56d0e
Hermawan Prajono <rafael.hermawan14@gmail.com>
2023-10-06 12:05:25 +0800

    fix vite static site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rhapael14.github.io/my-note-app__IDCAMP-2023/",
+  base: "/my-note-app__IDCAMP-2023/",
 })
